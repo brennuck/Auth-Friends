fca3c5494c3610aa1b0784db9aaed84f5fb6ee08
Brennon Nuckols <brennuck@gmail.com>
2020-02-08 16:42:05 -0700

    Added router

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import Login from './Components/Login';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./Components/PrivateRoute";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Switch>
+        <PrivateRoute exact path="/friends" component={FriendsList} />
+        <Route path="/login" component={Login} />
+        <Route component={Login} />
+      </Switch>
     </div>
   );
 }
